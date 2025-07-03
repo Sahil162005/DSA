e02a289f2b835a06c42c9efc682ed19682cb4b9b
Sahil Yadav <yadavsahil1605@gmail.com>
2025-07-03 18:34:57 +0530

    Time: 3 ms (5.89%), Space: 10.7 MB (46.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    int solve(vector<int>& dp,vector<int>& nums,int n){
+         if (n < 0) return 0;
+        if(dp[n]!=-1) return dp[n];
+
+        dp[n]=max(solve(dp,nums,n-1),solve(dp,nums,n-2)+nums[n]);
+        return dp[n];
+    }
     int rob(vector<int>& nums) {
         // tabulation
         // int n=nums.size();
@@ -14,18 +21,27 @@ public:
 
         // }
         // return dp[n-1];
+
         //optimized S.C
+        // int n=nums.size();
+        // if(n==1){
+        //     return nums[0];
+        // }
+        // int prev1=nums[0];
+        // int prev2=max(nums[0],nums[1]);
+        // for(int i=2;i<n;i++){
+        //     int curr =max(prev2,prev1+nums[i]);
+        //     prev1=prev2;
+        //     prev2=curr;
+        // }
+        // return prev2;
+
+        //Memoization
         int n=nums.size();
         if(n==1){
             return nums[0];
         }
-        int prev1=nums[0];
-        int prev2=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            int curr =max(prev2,prev1+nums[i]);
-            prev1=prev2;
-            prev2=curr;
-        }
-        return prev2;
+        vector<int>dp(n+1,-1);
+        return solve(dp,nums,n-1);
     }
 };
