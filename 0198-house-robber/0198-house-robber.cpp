6eb88f0bdc676fed59c1b24f742878482b2f8209
Sahil Yadav <yadavsahil1605@gmail.com>
2025-07-03 18:24:53 +0530

    Time: 0 ms (100%), Space: 10.2 MB (79.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        // tabulation
+        // int n=nums.size();
+        // if(n==1){
+        //     return nums[0];
+        // }
+        // vector<int>dp(n+1,0);
+        // dp[0]=nums[0];
+        // dp[1] = max(nums[0], nums[1]);;
+        // for(int i=2;i<n;i++){
+        //     dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+
+        // }
+        // return dp[n-1];
+        //optimized S.C
         int n=nums.size();
         if(n==1){
             return nums[0];
         }
-        vector<int>dp(n+1,0);
-        dp[0]=nums[0];
-        dp[1] = max(nums[0], nums[1]);;
+        int prev1=nums[0];
+        int prev2=max(nums[0],nums[1]);
         for(int i=2;i<n;i++){
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
-
+            int curr =max(prev2,prev1+nums[i]);
+            prev1=prev2;
+            prev2=curr;
         }
-        return dp[n-1];
-
-       
-        
+        return prev2;
     }
 };
