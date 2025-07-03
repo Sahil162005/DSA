9ccf6508691e12c457fddee8f5e1d043ab3543ad
Sahil Yadav <yadavsahil1605@gmail.com>
2025-07-03 19:45:31 +0530

    Time: 0 ms (100%), Space: 10.3 MB (65.7%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int start,int end){
-        int len = end - start + 1;
-        if (len == 1) return nums[start];
-         vector<int> dp(len);
-        dp[0]=nums[start];
-        dp[1]=max(nums[start],nums[start+1]);
-        for(int i=2;i<len;i++){
-            dp[i] = max(dp[i-1], dp[i-2] + nums[start+i]);
+ // Tabulation Solution
+    // int solve(vector<int>& nums,int start,int end){
+    //     int len = end - start + 1;
+    //     if (len == 1) return nums[start];
+    //      vector<int> dp(len);
+    //     dp[0]=nums[start];
+    //     dp[1]=max(nums[start],nums[start+1]);
+    //     for(int i=2;i<len;i++){
+    //         dp[i] = max(dp[i-1], dp[i-2] + nums[start+i]);
 
+    //     }
+    //     return dp[len-1];
+    // }
+    int solve(vector<int>& nums,int start,int end){
+         int prev1=nums[start];
+         if(end-start+1==1){
+            return nums[start];
+         }
+        int prev2=max(nums[start],nums[start+1]);
+        for(int i=start+2;i<=end;i++){
+            int curr =max(prev2,prev1+nums[i]);
+            prev1=prev2;
+            prev2=curr;
         }
-        return dp[len-1];
+        return prev2;
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
