92e831b37451eefe9134c64ec4fd67669e402e62
Sahil Yadav <yadavsahil1605@gmail.com>
2025-07-05 02:39:49 +0530

    Time: 2 ms (18.94%), Space: 9.3 MB (33.66%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
 //    1) Recursive Solution
-    // bool ispossible(int m,int n,int row,int col){
-    //     if(row<0 || row>=m || col<0 || col>=n){
-    //         return false;
-    //     }
-    //     return true;
+    bool ispossible(int m,int n,int row,int col){
+        if(row<0 || row>=m || col<0 || col>=n){
+            return false;
+        }
+        return true;
 
-    // }
+    }
     // int fn(int m,int n,int row,int col){
     //     if(row==m-1 && col==n-1){
     //         return 1;
@@ -26,7 +26,21 @@ public:
     //     return fn(m,n,0,0);
     // }
 // 2.)Memoization
-//   
+//    int fn(int m,int n,int row,int col,vector<vector<int>>&dp){
+//     if(row < 0 || col < 0) return 0;
+//     if(row==0 && col==0){
+//         return 1;
+//     }
+//     if(dp[row][col]!=-1) return dp[row][col];
+//     dp[row][col]= fn(m,n,row-1,col,dp) + fn(m,n,row,col-1,dp);
+//      return dp[row][col];
+//    }
+
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>>dp(m,vector<int>(n,-1));
+//         return fn(m,n,m-1,n-1,dp);
+        
+//     }
 // 3.) Tabulation
      int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
@@ -44,4 +58,5 @@ public:
         }
         return dp[m-1][n-1];        
     }
+
 };
