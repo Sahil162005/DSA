e1686502b84c6e15997c7b44004c5ac97900302e
Sahil Yadav <yadavsahil1605@gmail.com>
2025-07-05 02:36:15 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.82%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -26,19 +26,22 @@ public:
     //     return fn(m,n,0,0);
     // }
 // 2.)Memoization
-   int fn(int m,int n,int row,int col,vector<vector<int>>&dp){
-    if(row < 0 || col < 0) return 0;
-    if(row==0 && col==0){
-        return 1;
-    }
-    if(dp[row][col]!=-1) return dp[row][col];
-    dp[row][col]= fn(m,n,row-1,col,dp) + fn(m,n,row,col-1,dp);
-     return dp[row][col];
-   }
-
-    int uniquePaths(int m, int n) {
+//   
+// 3.) Tabulation
+     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return fn(m,n,m-1,n-1,dp);
-        
+        dp[0][0]=1;
+        for(int i=1;i<n;i++){
+            dp[0][i]=1;
+        }
+        for(int i=1;i<m;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];        
     }
 };
