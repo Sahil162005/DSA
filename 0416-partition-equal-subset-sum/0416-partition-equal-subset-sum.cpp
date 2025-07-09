896e6fef5f04ae9f27bb1b298ae273c486efc3ca
Sahil Yadav <yadavsahil1605@gmail.com>
2025-07-09 22:46:28 +0530

    Time: 133 ms (52.66%), Space: 110.6 MB (32.09%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-   bool fn(int index,int sum,int totalsum,vector<int>&nums,vector<vector<int>>&dp){
-    if(sum==totalsum-sum) return true;
-    if(index==0) return false;
+   bool fn(int index,int sum,vector<int>&nums,vector<vector<int>>&dp){
+    if(sum==0) return true;
+    if(index==0) return (nums[0]==sum);
     if(dp[index][sum]!=-1) return dp[index][sum];
-    bool nt=fn(index-1,sum,totalsum,nums,dp);
+    bool nt=fn(index-1,sum,nums,dp);
     bool t=false;
-    if(nums[index]<=totalsum){
-        t=fn(index-1,sum+nums[index],totalsum,nums,dp);
+    if(nums[index]<=sum){
+        t=fn(index-1,sum-nums[index],nums,dp);
     }
     dp[index][sum]= t||nt;
     return dp[index][sum];
@@ -18,7 +18,9 @@ public:
         for(int i=0;i<n;i++){
             sum+=nums[i];
         }
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        return fn(n-1,0,sum,nums,dp);
+        if(sum%2 !=0) return false;
+        int sum1=sum/2;
+        vector<vector<int>>dp(n,vector<int>(sum1+1,-1));
+        return fn(n-1,sum1,nums,dp);
     }
 };
