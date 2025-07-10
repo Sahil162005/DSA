10ca782ae2b31d44b3ac937cca155dd67c49ff50
Sahil Yadav <yadavsahil1605@gmail.com>
2025-07-10 19:00:00 +0530

    Time: 47 ms (36.81%), Space: 49.4 MB (29.87%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int fn(int ind,vector<int>&coins,int amount,vector<vector<int>>&dp){
+   int fn(int ind,int amount,vector<int>& coins,vector<vector<int>>&dp){
         if(amount==0) return 0;
         if(ind==0){
-            if (amount % coins[ind] == 0) return amount / coins[0];
-            return 1e9; 
+            if(amount%coins[ind]==0) return amount/coins[ind];
+            return 1e9;
         }
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int nt=fn(ind-1,coins,amount,dp);
-        int t=1e9;
+        if(dp[ind][amount]!=-1)return dp[ind][amount];
+        int nottake=fn(ind-1,amount,coins,dp);
+        int take=1e9;
         if(coins[ind]<=amount){
-            t=1+fn(ind,coins,amount-coins[ind],dp);
+            take=1+fn(ind,amount-coins[ind],coins,dp);
         }
-        return dp[ind][amount]=min(t,nt);
-        
+        return dp[ind][amount]=min(take,nottake);
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-        int res= fn(coins.size()-1,coins,amount,dp);
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int res=fn(n-1,amount,coins,dp);
         if(res==1e9) return -1;
         return res;
         
