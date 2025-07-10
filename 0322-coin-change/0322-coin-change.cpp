a7707a1c871d70e16a3b16ff407727bb77d17191
Sahil Yadav <yadavsahil1605@gmail.com>
2025-07-10 19:11:32 +0530

    Time: 44 ms (41.83%), Space: 48.8 MB (46.67%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -16,8 +16,27 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int res=fn(n-1,amount,coins,dp);
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        // int res=fn(n-1,amount,coins,dp);
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+        }
+    for(int j=0;j<=amount;j++){
+        if(j%coins[0]==0) dp[0][j]=j/coins[0];
+        else dp[0][j]=1e9;
+    }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                 int nottake=dp[i-1][j];
+                 int take=1e9;
+                  if(coins[i]<=j){
+                  take=1+dp[i][j-coins[i]];
+                   }
+                dp[i][j]=min(take,nottake);
+            }
+        }
+        int res=dp[n-1][amount];
+
         if(res==1e9) return -1;
         return res;
         
