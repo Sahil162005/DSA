ba44ecd8b8c497eec8159158d1e38bd31c7e3afa
Sahil Yadav <yadavsahil1605@gmail.com>
2025-07-11 21:32:40 +0530

    Time: 31 ms (26.43%), Space: 46.2 MB (11.09%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -16,7 +16,27 @@ public:
     }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return fn(n-1,amount,coins,dp);
+        if (coins.empty()) return 0;
+        vector<vector<unsigned long long>>dp(n,vector<unsigned long long>(amount+1,0));
+        for(int i=0;i<n;i++){
+        dp[i][0]=1;
+        }
+        for (int j = 0; j <= amount; j++) {
+       if (j % coins[0] == 0) dp[0][j] = 1;
+          }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                unsigned long long nottake=dp[i-1][j];
+        unsigned long long take=0;
+        if(coins[i]<=amount){
+            if(coins[i]<=j){
+             take=dp[i][j-coins[i]];
+            }
+        }
+       dp[i][j]=(take+nottake);
+            }
+        }
+        return dp[n-1][amount];
+        
     }
 };
