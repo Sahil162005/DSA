5ead72cabe06d77e8e2ba0073b8104999618b748
Sahil Yadav <yadavsahil1605@gmail.com>
2025-07-14 01:16:26 +0530

    Time: 34 ms (31.63%), Space: 23.3 MB (96.94%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,38 +1,67 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+    // int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+    //     if(grid[0][0]==1) return -1;
+    //     queue<pair<pair<int,int>,int>>q;
+    //     q.push({{0,0},1});
+    //     int dcol[]={0,-1,0,1,1,1,-1,-1};
+    //     int drow[]={1,0,-1,0,1,-1,-1,1};
+    //     int n=grid.size();
+    //     vector<vector<int>>vis(n,vector<int>(n,1));
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<n;j++){
+    //             if(grid[i][j]==0) vis[i][j]=0;
+    //         }
+    //     }
+    //     int cnt=INT_MAX;
+    //     while(!q.empty()){
+    //         int x=q.front().first.first;
+    //         int y=q.front().first.second;
+    //         int dis=q.front().second;
+    //         q.pop();
+    //         if(x==n-1 && y==n-1){
+    //             cnt=min(dis,cnt);
+    //         }
+    //         for(int k=0;k<8;k++){
+    //             int row=x+drow[k];
+    //             int col=y+dcol[k];
+    //             if(row>=0 && row<n && col>=0 && col<n && vis[row][col]!=1){
+    //                 vis[row][col]=1;
+    //                 q.push({{row,col},dis+1});
+    //             }
+    //         }
+
+    //     }
+    //     if(cnt==INT_MAX) return -1;
+    //     return cnt;
+    // }
+     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         if(grid[0][0]==1) return -1;
-        queue<pair<pair<int,int>,int>>q;
+        priority_queue<pair<pair<int,int>,int>, vector<pair<pair<int,int>,int>>, greater<pair<pair<int,int>,int>>> q;
         q.push({{0,0},1});
         int dcol[]={0,-1,0,1,1,1,-1,-1};
         int drow[]={1,0,-1,0,1,-1,-1,1};
         int n=grid.size();
-        vector<vector<int>>vis(n,vector<int>(n,1));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==0) vis[i][j]=0;
-            }
-        }
-        int cnt=INT_MAX;
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        dist[0][0]=1;
         while(!q.empty()){
-            int x=q.front().first.first;
-            int y=q.front().first.second;
-            int dis=q.front().second;
+            int x=q.top().first.first;
+            int y=q.top().first.second;
+            int dis=q.top().second;
             q.pop();
-            if(x==n-1 && y==n-1){
-                cnt=min(dis,cnt);
-            }
             for(int k=0;k<8;k++){
                 int row=x+drow[k];
                 int col=y+dcol[k];
-                if(row>=0 && row<n && col>=0 && col<n && vis[row][col]!=1){
-                    vis[row][col]=1;
+                if(row>=0 && row<n && col>=0 && col<n && grid[row][col]==0){
+                    if(dist[row][col]>dis+1){
+                        dist[row][col]=dis+1; 
                     q.push({{row,col},dis+1});
+                    }
                 }
             }
 
         }
-        if(cnt==INT_MAX) return -1;
-        return cnt;
+        if(dist[n-1][n-1]==INT_MAX) return -1;
+        return dist[n-1][n-1];
     }
 };
