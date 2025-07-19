0d94440f536780133662c2d5c27176460660c97f
Sahil Yadav <yadavsahil1605@gmail.com>
2025-07-19 18:38:19 +0530

    Time: 7 ms (41.78%), Space: 23.9 MB (57.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
+    // vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    //     vector<vector<int>>ans;
+    //     int n=intervals.size();
+    //     sort(intervals.begin(),intervals.end());
+    //     int index=-1;
+    //     for(int i=0;i<n;i++){
+    //         int first=intervals[i][0];
+    //         int second=intervals[i][1];
+    //         if(! ans.empty() && second<=ans.back()[1]){
+    //             continue;
+    //         }
+    //         for(int j=i+1;j<n;j++){
+    //             if(intervals[j][0]<=second){
+    //                 second=max(second,intervals[j][1]);
+    //             }
+    //             else{
+    //                 break;
+    //             }
+    //         }
+    //         ans.push_back({first,second});
+            
+    //     }
+    //     return ans;
+    // }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>>ans;
         int n=intervals.size();
         sort(intervals.begin(),intervals.end());
         int index=-1;
         for(int i=0;i<n;i++){
-            int first=intervals[i][0];
-            int second=intervals[i][1];
-            if(! ans.empty() && second<=ans.back()[1]){
-                continue;
+            if(ans.empty()||intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
             }
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0]<=second){
-                    second=max(second,intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else if(intervals[i][0]<=ans.back()[1]){
+                ans.back()[1]=max(intervals[i][1],ans.back()[1]);
             }
-            ans.push_back({first,second});
             
         }
         return ans;
