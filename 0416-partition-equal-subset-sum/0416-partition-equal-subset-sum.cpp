747663b1d246d8d450fb24003538811d9e96ee2d
Sahil Yadav <yadavsahil1605@gmail.com>
2025-08-03 23:30:14 +0530

    Time: 123 ms (64.74%), Space: 110.3 MB (45.41%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
-   bool fn(int index,int sum,vector<int>&nums,vector<vector<int>>&dp){
-    if(sum==0) return true;
-    if(index==0) return (nums[0]==sum);
-    if(dp[index][sum]!=-1) return dp[index][sum];
-    bool nt=fn(index-1,sum,nums,dp);
-    bool t=false;
-    if(nums[index]<=sum){
-        t=fn(index-1,sum-nums[index],nums,dp);
-    }
-    dp[index][sum]= t||nt;
-    return dp[index][sum];
-   }
+    bool fn(vector<int>& arr, int sum,int index,vector<vector<int>>&dp){
+    if(sum==0){
+          return true;
+      }
+    if (sum < 0) return false;
+      if (index == 0) return arr[0] == sum;
+      if(dp[index][sum]!=-1) return dp[index][sum];
+      bool skip=fn(arr,sum,index-1,dp);
+      bool take=false;
+      if(arr[index]<=sum){
+          take=fn(arr,sum-arr[index],index-1,dp);
+      }
+      return dp[index][sum]= skip||take;
+  }
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
         int sum=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.size();i++){
             sum+=nums[i];
         }
-        if(sum%2 !=0) return false;
-        int sum1=sum/2;
-        vector<vector<bool>>dp(n,vector<bool>(sum1+1,0));
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;
-        }
-        if(nums[0]<=sum1){
-        dp[0][nums[0]]=true;}
-        for(int index=1;index<n;index++){
-            for(int j=1;j<=sum1;j++){
-                bool nt=dp[index-1][j];
-                bool t=false;
-               if(nums[index]<=j){
-               t=dp[index-1][j-nums[index]];
-                }
-               dp[index][j]= t||nt;
-            }
-        }
-        return dp[n-1][sum1];
+        if(sum%2!=0)return false;
+        vector<vector<int>>dp(nums.size(),vector<int>(sum/2+1,-1));
+        return fn(nums,sum/2,nums.size()-1,dp);
     }
 };
