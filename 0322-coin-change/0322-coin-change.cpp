fca98480640e69c509f9da22d74b8cfd9b2cf327
Sahil Yadav <yadavsahil1605@gmail.com>
2025-08-20 00:18:25 +0530

    Time: 39 ms (52.23%), Space: 49.4 MB (30.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
-   int fn(int ind,int amount,vector<int>& coins,vector<vector<int>>&dp){
-        if(amount==0) return 0;
-        if(ind==0){
-            if(amount%coins[ind]==0) return amount/coins[ind];
-            return 1e9;
-        }
-        if(dp[ind][amount]!=-1)return dp[ind][amount];
-        int nottake=fn(ind-1,amount,coins,dp);
-        int take=1e9;
-        if(coins[ind]<=amount){
-            take=1+fn(ind,amount-coins[ind],coins,dp);
-        }
-        return dp[ind][amount]=min(take,nottake);
+   int fn(vector<int>& coins, int amount,vector<vector<int>>&dp,int index){
+    if(amount==0){
+        return 0;
     }
+    if(index==0){
+        if(amount%coins[0]==0){
+            return amount/coins[0];
+        }
+        return 1e8;
+    }
+    if(dp[index][amount]!=-1)return dp[index][amount];
+    int nottake=fn(coins,amount,dp,index-1);
+    int take=1e8;
+    if(coins[index]<=amount){
+        take=1+fn(coins,amount-coins[index],dp,index);
+    }
+    return dp[index][amount]=min(take,nottake);
+   }
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
-        // int res=fn(n-1,amount,coins,dp);
-        for(int i=0;i<n;i++){
-            dp[i][0]=0;
-        }
-    for(int j=0;j<=amount;j++){
-        if(j%coins[0]==0) dp[0][j]=j/coins[0];
-        else dp[0][j]=1e9;
-    }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                 int nottake=dp[i-1][j];
-                 int take=1e9;
-                  if(coins[i]<=j){
-                  take=1+dp[i][j-coins[i]];
-                   }
-                dp[i][j]=min(take,nottake);
-            }
-        }
-        int res=dp[n-1][amount];
-
-        if(res==1e9) return -1;
-        return res;
-        
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+        int res= fn(coins,amount,dp,coins.size()-1);
+        if(res>=1e8)return -1;
+        return res;  
     }
 };
