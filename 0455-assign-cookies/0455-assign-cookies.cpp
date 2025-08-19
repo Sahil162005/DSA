ae7d3720f1fcadf23ba1f3e8858960ee892a1707
Sahil Yadav <yadavsahil1605@gmail.com>
2025-08-19 23:38:55 +0530

    Time: 10 ms (47.73%), Space: 44.9 MB (43.56%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    int findContentChildren(vector<int>& g, vector<int>& s) {
+    // int fn(vector<int>& g, vector<int>& s,int index,int cnt,vector<vector<int>>&dp){
+    //     if(index==g.size()|| cnt==s.size()){
+    //         return 0;
+    //     }
+    //      if(dp[index][cnt]!=-1)return dp[index][cnt];
+    //     int maxi=0;
+    //     int nottake=0+fn(g,s,index,cnt+1,dp);
+    //     int take=0;
+    //     if(g[index]<=s[cnt]){
+    //         take=1+fn(g,s,index+1,cnt+1,dp);
+    //     }
+    //     maxi=max(take,nottake);
+    //     return dp[index][cnt]=maxi;
+    // }
+    // int findContentChildren(vector<int>& g, vector<int>& s) {
+    //     sort(g.begin(), g.end());
+    //    sort(s.begin(), s.end());
+    //     vector<vector<int>>dp(g.size(),vector<int>(s.size(),-1));
+    //     return fn(g,s,0,0,dp);
+    // }
+    int findContentChildren(vector<int>& g, vector<int>& s){
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int i=0,j=0;
         int cnt=0;
-        sort(s.begin(),s.end());
-        sort(g.begin(),g.end());
-        int i=0;
-        int j=0;
         while(i<g.size() && j<s.size()){
-            if(g[i]<=s[j]){
+            if(s[j]>=g[i]){
                 cnt++;
                 i++;
                 j++;
             }
-            else if(g[i]>s[j]){
+            else{
                 j++;
             }
-            else{
-                i++;
-            }
-        }
+        } 
         return cnt;
-        
-        
     }
 };
