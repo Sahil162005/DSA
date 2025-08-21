1b97776c36cb57979e071eaf5c7c530747387f5e
Sahil Yadav <yadavsahil1605@gmail.com>
2025-08-22 03:22:36 +0530

    Time: 91 ms (38.38%), Space: 75.9 MB (43.88%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -23,12 +23,12 @@ public:
         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                int match=0;
                 if(s[i-1]==s2[j-1]){
-                    match=1+dp[i-1][j-1];
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
-                int notmatch=max(dp[i-1][j],dp[i][j-1]);
-                dp[i][j]=max(match,notmatch);
+                else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
         return dp[n][n];
