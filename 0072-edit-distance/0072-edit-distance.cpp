7f8c40239cc032a2d43a757e5a2cb0a74cbaaca1
Sahil Yadav <yadavsahil1605@gmail.com>
2025-08-23 22:39:18 +0530

    Time: 10 ms (34.77%), Space: 13.3 MB (29.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -18,12 +18,35 @@ public:
         int notmatch=min({rep,del,ins});
         return dp[ind1][ind2]=min(match,notmatch);
     } 
+    // int minDistance(string word1, string word2) {
+    //     int n=word1.size();
+    //     int m=word2.size();
+    //     vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+    //     return fn(word1,word2,n,m,dp);
+
+    // }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return fn(word1,word2,n,m,dp);
-
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int match=1e9;
+                if(word1[i-1]==word2[j-1]){
+                    match=dp[i-1][j-1];
+                }
+                int notmatch=min({1+dp[i-1][j-1],1+dp[i-1][j],1+dp[i][j-1]});
+                dp[i][j]=min(match,notmatch);
+            }
+        }
+        return dp[n][m];
+        
 
     }
 };
