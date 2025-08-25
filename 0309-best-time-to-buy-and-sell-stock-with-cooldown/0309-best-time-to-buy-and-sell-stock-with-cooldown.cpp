234cb59e46229c378c04fef5f7b3a3b78f33501f
Sahil Yadav <yadavsahil1605@gmail.com>
2025-08-25 10:58:07 +0530

    Time: 1 ms (49.05%), Space: 15 MB (88.91%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -25,30 +25,57 @@ public:
     //     vector<vector<int>>dp(n,vector<int>(2,-1));
     //     return fn(prices,0,1,dp);
     // }
+    // int maxProfit(vector<int>& prices) {
+    //     int n=prices.size();
+    //     vector<vector<int>>dp(n+2,vector<int>(2,0));
+    //     for(int ind=n-1;ind>=0;ind--){
+    //         for(int canbuy=0;canbuy<=1;canbuy++){
+    //             int profit=0;
+    //     if(canbuy==1){
+    //         int buy=-prices[ind]+dp[ind+1][0];
+    //         int skip=dp[ind+1][canbuy];
+    //         profit = max(buy, skip);
+    //     }
+    //     else{
+    //         int sell=prices[ind]+dp[ind+2][1];
+    //         int skip=dp[ind+1][canbuy];
+    //         profit = max(sell, skip);
+
+    //     }
+        
+    //     dp[ind][canbuy]=profit;
+
+    //         }
+    //     }
+    //     return dp[0][1];
+    // }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n+2,vector<int>(2,0));
+        vector<int>prev(2,0);
+        vector<int>prev1(2,0);
+        vector<int>curr(2,0);
         for(int ind=n-1;ind>=0;ind--){
             for(int canbuy=0;canbuy<=1;canbuy++){
                 int profit=0;
         if(canbuy==1){
-            int buy=-prices[ind]+dp[ind+1][0];
-            int skip=dp[ind+1][canbuy];
+            int buy=-prices[ind]+prev[0];
+            int skip=prev[canbuy];
             profit = max(buy, skip);
         }
         else{
-            int sell=prices[ind]+dp[ind+2][1];
-            int skip=dp[ind+1][canbuy];
+            int sell=prices[ind]+prev1[1];
+            int skip=prev[canbuy];
             profit = max(sell, skip);
 
         }
         
-        dp[ind][canbuy]=profit;
+        curr[canbuy]=profit;
 
             }
+            prev1=prev;
+            prev=curr;
         }
-        return dp[0][1];
-        
+        return curr[1];
     }
 
 };
