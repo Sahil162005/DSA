37bd0f896842092d3f6164b492b67b98c030a5ed
Sahil Yadav <yadavsahil1605@gmail.com>
2025-08-25 09:22:38 +0530

    Time: 12 ms (39.09%), Space: 18.2 MB (48.39%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int fn(vector<int>&prices,int ind, vector<vector<vector<int>>>&dp,int canbuy,int transactions,int k){
+    int fn(vector<int>&prices,int ind, vector<vector<vector<int>>>&dp,int canbuy,int transactions){
         if(ind==prices.size()){
+            return 0;
+        }
+        if(transactions==0){
             return 0;
         }
         if(dp[ind][canbuy][transactions]!=-1)return dp[ind][canbuy][transactions];
         int profit = 0;
         
-        if(canbuy == 1 && transactions<k) {
-            int buy = -prices[ind] + fn(prices, ind + 1, dp, 0,transactions,k); 
-            int skip = fn(prices, ind + 1, dp, 1,transactions,k);               
+        if(canbuy == 1 ) {
+            int buy = -prices[ind] + fn(prices, ind + 1, dp, 0,transactions); 
+            int skip = fn(prices, ind + 1, dp, 1,transactions);               
             profit = max(buy, skip);
         }
-        else if(transactions<k) {
-            int sell = prices[ind] + fn(prices, ind + 1, dp, 1,transactions+1,k); 
-            int skip = fn(prices, ind + 1, dp, 0,transactions,k); 
+        else{
+            int sell = prices[ind] + fn(prices, ind + 1, dp, 1,transactions-1); 
+            int skip = fn(prices, ind + 1, dp, 0,transactions); 
             profit = max(sell, skip);
         }
          return dp[ind][canbuy][transactions] = profit;
@@ -22,6 +25,6 @@ public:
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
            vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
-           return fn(prices,0,dp,1,0,k);
+           return fn(prices,0,dp,1,k);
     }
 };
