24d3c9a5ce60de351d3ddeef944829448f783f86
Sahil Yadav <yadavsahil1605@gmail.com>
2025-08-26 23:22:15 +0530

    Time: 67 ms (61.98%), Space: 14.5 MB (52.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -18,24 +18,38 @@ public:
     //     vector<vector<int>>dp(n,vector<int>(n+1,-1));
     //     return fn(nums,0,-1,dp);
     // }
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        vector<int>pre(n+1,0);
-        vector<int>curr(n+1,0);
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int n=nums.size();
+    //     // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    //     vector<int>pre(n+1,0);
+    //     vector<int>curr(n+1,0);
 
-        for(int ind=n-1;ind>=0;ind--){
-            for(int prev=ind-1;prev>=-1;prev--){
-                 int pick=0;
-        if(prev==-1||nums[ind]>nums[prev]){
-            pick=1+pre[ind+1];
-        }
-        int notpick=0+pre[prev+1];
-        curr[prev+1]=max(pick,notpick);
+    //     for(int ind=n-1;ind>=0;ind--){
+    //         for(int prev=ind-1;prev>=-1;prev--){
+    //              int pick=0;
+    //     if(prev==-1||nums[ind]>nums[prev]){
+    //         pick=1+pre[ind+1];
+    //     }
+    //     int notpick=0+pre[prev+1];
+    //     curr[prev+1]=max(pick,notpick);
+    //         }
+    //         pre=curr;
+    //     }
+    //     return pre[0];
+    // }
+     int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+            if(nums[i]>nums[j]){
+                dp[i]=max(dp[i],dp[j]+1);
             }
-            pre=curr;
+               maxi=max(maxi,dp[i]);
+            }
         }
-        return pre[0];
+        return maxi;
     }
 
 };
