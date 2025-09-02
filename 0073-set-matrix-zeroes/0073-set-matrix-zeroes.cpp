4dc2fe5cc0248f335abe00d17478be54f0bfbc6f
Sahil Yadav <yadavsahil1605@gmail.com>
2025-09-03 00:34:55 +0530

    Time: 0 ms (100%), Space: 18.4 MB (95.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -28,6 +28,12 @@ public:
         int n=matrix[0].size();
         bool rowz=false;
         bool colz=false;
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0){
+                rowz=true;
+                break;
+            }
+        }
         for(int i=0;i<n;i++){
             if(matrix[0][i]==0){
                 colz=true;
@@ -35,29 +41,25 @@ public:
             }
         }
         for(int i=0;i<m;i++){
-            if(matrix[i][0]==0){
-                rowz=true;
-                break;
-            }
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
+            for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
+                    matrix[0][j]=0;
                     matrix[i][0]=0;
-                   matrix[0][j]=0;
                 }
             }
         }
         for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[0][j]==0||matrix[i][0]==0){
+            if(matrix[i][0]==0){
+                for(int j=0;j<n;j++){
                     matrix[i][j]=0;
                 }
             }
         }
-        if(colz){
-            for(int i=0;i<n;i++){
-                matrix[0][i]=0;
+        for(int i=1;i<n;i++){
+            if(matrix[0][i]==0){
+                for(int j=0;j<m;j++){
+                    matrix[j][i]=0;
+                }
             }
         }
         if(rowz){
@@ -65,8 +67,12 @@ public:
                 matrix[i][0]=0;
             }
         }
-       
-       
+        if(colz){
+            for(int i=0;i<n;i++){
+                matrix[0][i]=0;
+            }
+        }
 
+       
     }
 };
