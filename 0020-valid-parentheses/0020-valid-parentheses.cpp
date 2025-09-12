a6110928b74f0e84cff251b326a2f4f4be405b27
Sahil Yadav <yadavsahil1605@gmail.com>
2025-09-12 23:31:53 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.96%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        bool result=true;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==')' || s[i]=='}' || s[i]==']'){
+                if(!st.empty() && ((s[i]==')' && st.top()=='(') || 
+                (s[i]=='}' && st.top()=='{')||
+                (s[i]==']' && st.top()=='['))){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+             
+            else{
+                st.push(s[i]);
+            }
+        }
+        if(!st.empty())return false;
+        return result;
+    }
+};
