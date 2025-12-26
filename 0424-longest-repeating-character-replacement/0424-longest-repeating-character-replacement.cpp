4c9ebb4c1488ec85d84a6d2fd3afdfbe113d724e
Sahil Yadav <yadavsahil1605@gmail.com>
2025-12-26 23:49:25 +0530

    Time: 16 ms (14.82%), Space: 10.9 MB (68.91%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -6,25 +6,22 @@ public:
         int maxlen=0;
         int maxfreq=0;
         int n=s.size();
-        int changes=0;
         while(r<n){
             mpp[s[r]-'A']++;
             if(mpp[s[r]-'A']>maxfreq){
                 maxfreq=mpp[s[r]-'A'];
             }
-            if(changes>k){
+            if((r-l+1-maxfreq)>k){
                 mpp[s[l]-'A']--;
-                int maxi=0;
-                for(auto it:mpp){
-                    maxi=max(maxi,it.second);
-                }
-                maxfreq=maxi;
+                // int maxi=0;
+                // for(auto it:mpp){
+                //     maxi=max(maxi,it.second);
+                // }
+                // maxfreq=maxi;
                 l++;
             }
-            int len=r-l+1;
-            changes=len-maxfreq;
-            if(changes<=k){
-                maxlen=max(maxlen,len);
+            if((r-l+1-maxfreq)<=k){
+                maxlen=max(maxlen,r-l+1);
             }
             r++;
         }
