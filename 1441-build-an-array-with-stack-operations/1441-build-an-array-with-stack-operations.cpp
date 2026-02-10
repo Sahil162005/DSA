cb3dd1b5b3e60ff05b9f87e242c1f8049d1f396a
Sahil Yadav <yadavsahil1605@gmail.com>
2026-02-10 22:35:38 +0530

    Time: 0 ms (100%), Space: 10.7 MB (85.19%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        stack<int>st;
         vector<string>arr;
         int j=0;
         for(int i=1;i<=n;i++){
