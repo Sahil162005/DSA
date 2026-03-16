b0f86924a4052d55c399bfa0b3b3ede43f77e84c
Sahil Yadav <yadavsahil1605@gmail.com>
2026-03-16 17:44:57 +0530

    Time: 11 ms (47.58%), Space: 11.5 MB (63.17%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n=s.size();
-        int m=t.size();
         unordered_map<char,int>mpp;
-        for(int i=0;i<m;i++){
+        for(int i=0;i<t.size();i++){
             mpp[t[i]]++;
         }
-        int si=0,l=0,r=0,cnt=0;
-        int minlen=INT_MAX;
-        while(r<n){
-            if(mpp[s[r]]>0)cnt++;
-            mpp[s[r]]--;
-            while(cnt==m){
-                if(r-l+1<minlen){
-                    minlen=r-l+1;
-                    si=l;
-                }
-                mpp[s[l]]++;
-                if(mpp[s[l]]>0){
-                    cnt--;
-                }
-                l++;
+        int cnt=t.size();
+        int c=0;
+        int l=0,r=0;
+        int start=-1;
+        int len=-1;
+        int minilen=INT_MAX;
+        while(r<s.size()){
+            if(mpp.find(s[r])!=mpp.end()){
+                mpp[s[r]]--;
+                if (mpp[s[r]] >= 0) c++;
             }
-            r++;
-            
-        }
-        if(minlen==INT_MAX){
+                while(cnt==c){
+                    if(r-l+1<minilen){
+                        minilen=r-l+1;
+                        start=l;
+                    }
+                    if(mpp.find(s[l])!=mpp.end()){
+                        mpp[s[l]]++;
+                        if(mpp[s[l]]>0){
+                        c--;
+                    }
+                    }
+                    l++;
+                }
+                r++;
+            }
+        if(minilen==INT_MAX){
             return "";
         }
-        return s.substr(si,minlen);
+        return s.substr(start,minilen);
     }
 };
