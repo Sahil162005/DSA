f4e227110cac6a2a1afbfe45f523987e44974316
Sahil Yadav <yadavsahil1605@gmail.com>
2026-04-11 02:09:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (32.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        /// tabulation
-        // dp[0]=1;
-        // dp[1]=1;
-       // for(int i=2;i<=n;i++){
-         //   dp[i]=dp[i-1]+dp[i-2];
-       // }
-        //return dp[n];
-        //
-        // optimized S.C. Tabulation 
-        //int prev1=1;
-        //int prev2=1;
-        //for(int i=2;i<=n;i++){
-          //  int curr=prev1+prev2;
-           // prev2=prev1;
-           // prev1=curr;
-       // }
-        //return prev1;
-        //Memoization 
-        int sol=solve(dp,n);
-        return sol;
-    }
-    int solve(vector<int>&dp,int n){
+    int fn(int n, vector<int>&dp){
         if(n<=1){
             return 1;
         }
         if(dp[n]!=-1){
             return dp[n];
         }
-        return dp[n]=solve(dp,n-1)+solve(dp,n-2);
+        int one= fn(n-1,dp);
+        int two= fn(n-2,dp);
+        int ans= one+two;
+        return dp[n]=ans;
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return fn(n,dp);
     }
 };
