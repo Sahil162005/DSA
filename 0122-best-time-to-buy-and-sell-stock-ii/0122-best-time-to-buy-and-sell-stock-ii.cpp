272a6db06acfd3446ba0a4ee861f4b3e701d8076
Sahil Yadav <yadavsahil1605@gmail.com>
2026-06-27 00:17:29 +0530

    Time: 0 ms (100%), Space: 19.9 MB (97.37%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int fn(vector<int>&prices,int ind,vector<vector<int>>&dp,int canbuy){
-        if(ind==prices.size()){
-            return 0;
-        }
-        if(dp[ind][canbuy]!=-1)return dp[ind][canbuy];
-        int profit = 0;
+    // int fn(vector<int>&prices,int ind,vector<vector<int>>&dp,int canbuy){
+    //     if(ind==prices.size()){
+    //         return 0;
+    //     }
+    //     if(dp[ind][canbuy]!=-1)return dp[ind][canbuy];
+    //     int profit = 0;
         
-        if(canbuy == 1) {
-            int buy = -prices[ind] + fn(prices, ind + 1, dp, 0); 
-            int skip = fn(prices, ind + 1, dp, 1);               
-            profit = max(buy, skip);
-        }
-        else {
-            int sell = prices[ind] + fn(prices, ind + 1, dp, 1); 
-            int skip = fn(prices, ind + 1, dp, 0); 
-            profit = max(sell, skip);
-        }
-         return dp[ind][canbuy] = profit;
-
-    }
+    //     if(canbuy == 1) {
+    //         int buy = -prices[ind] + fn(prices, ind + 1, dp, 0); 
+    //         int skip = fn(prices, ind + 1, dp, 1);               
+    //         profit = max(buy, skip);
+    //     }
+    //     else {
+    //         int sell = prices[ind] + fn(prices, ind + 1, dp, 1); 
+    //         int skip = fn(prices, ind + 1, dp, 0); 
+    //         profit = max(sell, skip);
+    //     }
+    //      return dp[ind][canbuy] = profit;
+    // }
     // int maxProfit(vector<int>& prices) {
     //     int n=prices.size();
     //     vector<vector<int>>dp(n,vector<int>(2,-1));
@@ -52,28 +51,52 @@ public:
     //     }
     //     return dp[0][1];
     // }
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<int>prev(2,0);
-        vector<int>curr(2,0);
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=1;j++){
-                int profit = 0;
-                if(j==1){
-                    int buy = -prices[i] + prev[0];
-                    int skip = prev[1];             
-                     profit = max(buy, skip);
-                }
-                else{
-                    int sell=prices[i]+prev[1];
-                    int skip=prev[0];
-                    profit = max(sell, skip);
-                }
-                curr[j]=profit;
+    // int maxProfit(vector<int>& prices) {
+    //     int n=prices.size();
+    //     vector<int>prev(2,0);
+    //     vector<int>curr(2,0);
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=0;j<=1;j++){
+    //             int profit = 0;
+    //             if(j==1){
+    //                 int buy = -prices[i] + prev[0];
+    //                 int skip = prev[1];             
+    //                  profit = max(buy, skip);
+    //             }
+    //             else{
+    //                 int sell=prices[i]+prev[1];
+    //                 int skip=prev[0];
+    //                 profit = max(sell, skip);
+    //             }
+    //             curr[j]=profit;
 
+    //         }
+    //         prev=curr;
+    //     }
+    //    return curr[1];
+    // }
+    int maxProfit(vector<int>& prices){
+        int n = prices.size();
+        if (n < 2) return 0;
+        int mini = prices[0];
+        int maxi = prices[0];
+        int total = 0;
+        for(int i=1;i<n;i++){
+            if(prices[i]<maxi){
+                total += maxi - mini;
+                mini = prices[i];
+                maxi = prices[i];
+                continue;
             }
-            prev=curr;
+            if(prices[i]<mini){
+                mini = prices[i];
+                maxi = mini;
+                continue;
+            }
+            maxi = max(maxi,prices[i]);
         }
-       return curr[1];
+        total += maxi - mini;
+        return total;
     }
+    
 };
