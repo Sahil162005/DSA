d6630781dc0eb0dee5c90a2cffde3fb07df2ed42
Sahil Yadav <yadavsahil1605@gmail.com>
2026-06-26 20:34:29 +0530

    Time: 0 ms (100%), Space: 20.7 MB (37.44%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
-    int fn(vector<int>&nums , int ind,vector<int>&dp){
-        if(ind >= nums.size()-1){
-            return 0;
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        int count = 1e9;
-        int jumps = nums[ind];
-        for(int i = ind+1; i<=ind+jumps;i++){
-            if(i <= nums.size()-1){
-            count = min(1+fn(nums,i,dp),count) ;
+    // int fn(vector<int>&nums , int ind,vector<int>&dp){
+    //     if(ind >= nums.size()-1){
+    //         return 0;
+    //     }
+    //     if(dp[ind]!=-1){
+    //         return dp[ind];
+    //     }
+    //     int count = 1e9;
+    //     int jumps = nums[ind];
+    //     for(int i = ind+1; i<=ind+jumps;i++){
+    //         if(i <= nums.size()-1){
+    //         count = min(1+fn(nums,i,dp),count) ;
+    //         }
+    //     }
+    //     return dp[ind]=count;
+    // }
+    // int jump(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int>dp(n+1,-1);
+    //     return fn(nums,0,dp);
+    // }
+    int jump(vector<int>&nums){
+        int n = nums.size();
+        int maxi = 0;
+        int total =0;
+        int end = 0;
+        for(int i=0;i<n - 1;i++){
+            maxi = max(maxi,i+nums[i]);
+            if(i==end){
+                total++;
+                end = maxi;
             }
         }
-        return dp[ind]=count;
-    }
-    int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>dp(n+1,-1);
-        return fn(nums,0,dp);
+        return total;
     }
 };
