c36219cbbb135345ae776489cd802025b036f95c
Sahil Yadav <yadavsahil1605@gmail.com>
2026-07-26 12:00:22 +0530

    Time: 0 ms (100%), Space: 10.8 MB (52.13%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    int fn(vector<int>& nums,int ind,int last,vector<int>&dp){
-        if(ind>last){
-            return 0;
-        }
-        if(ind==last){
-            return nums[ind];
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        int take = nums[ind] + fn(nums,ind+2,last,dp);
-        int nottake= fn(nums,ind+1,last,dp);
-        return dp[ind]=max(take,nottake);
+   int fn(vector<int>&nums,int i,int end,vector<int>&dp){
+    if(i==end){
+        return nums[end];
     }
+    if(i > end){
+        return 0;
+    }
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+    int ans = nums[i] + fn(nums,i+2,end,dp);
+    int skip = fn(nums,i+1,end,dp);
+    return dp[i] = max(ans,skip);
+   }
     int rob(vector<int>& nums) {
-        int n= nums.size();
+        int n = nums.size();
         if(n==1){
             return nums[0];
         }
         vector<int>dp1(n+1,-1);
         vector<int>dp2(n+1,-1);
-        return max(fn(nums,0,n-2,dp1),fn(nums,1,n-1,dp2));
+
+        int start1= fn(nums,0,n-2,dp1);
+        int start2 = fn(nums,1,n-1,dp2);
+        return max(start1,start2);
+        
     }
 };
